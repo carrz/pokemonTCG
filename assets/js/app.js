97b84c9dc6fde982472e007ec3b5f0e67519686b
carrz <carla.zumaran@gmail.com>
2018-02-28 01:13:48 -0300

    Arreglo de modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,43 @@
 const form = document.getElementById('search-form');
 let searchPokemon;
 
+fetch('https://api.pokemontcg.io/v1/cards?supertype=pokemon&types=dragon|fire')
+.then((response) => {
+  return response.json();
+})
+.then((data) => {
+  console.log(data);
+  for (let i=0; i < data.cards.length; i++) {
+    let pokemonPicture = data.cards[i].imageUrl;
+    let pokemonId = data.cards[i].id;
+    //console.log(pokemonPicture);
+    $('#response-container').append(
+      "<div class='col-lg-2'><button value='" + pokemonId + "' class='pokemonInfo' type='button' data-toggle='modal' data-target='#myModal'><img class='pokemon' src='" + pokemonPicture + "' /></button></div>"
+    )}
+
+    $('.pokemonInfo').click(function() {
+      $('.modal-title').empty(); 
+      $('.modal-body').empty(); 
+      const pokeId = $(this).val();
+      fetch(`https://api.pokemontcg.io/v1/cards/${pokeId}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((pokedata) => {
+        //console.log(pokedata);
+        let poke = pokedata.card;
+        $('.modal-title').append('<h3>' + poke.name + '</h3>')
+        $('.modal-body').append('<img src="' + poke.imageUrl +'" /><p><strong>Types:</strong> ' + poke.types[0] + '</p><p><strong>Subtype:</strong> ' + poke.subtype + '</p><p><strong>Series:</strong> ' + poke.series + '</p><p><strong>Set:</strong> ' + poke.set + '</p>');
+        })
+      .catch((err) => {
+          alert(err);
+      });
+      })
+})
+.catch((err) => {
+  alert(err);
+});
+
 form.addEventListener('submit', function(e) {
   e.preventDefault();
   // responseContainer.innerHTML = '';
@@ -10,35 +47,41 @@ form.addEventListener('submit', function(e) {
 })
 
 function getPokemon() {
-  const pokemonRequest = new XMLHttpRequest();
-  pokemonRequest.open('GET', `https://api.pokemontcg.io/v1/cards?name=${searchPokemon}`);
-  pokemonRequest.onload = addPokemonCard;
-  pokemonRequest.onerror = handleError;
-  pokemonRequest.send();
-}
-
-function handleError() {
-  console.log('Oh, Oh, tenemos un problema');
-}
-
-function addPokemonCard() {
+fetch(`https://api.pokemontcg.io/v1/cards?name=${searchPokemon}`)
+.then((response) => {
+  return response.json();
+})
+.then((data) => {
   $('#response-container').empty();
-  const data = JSON.parse(this.responseText);
-  console.log(data);
+  //console.log(data);
 
   for (let i=0; i < data.cards.length; i++) {
     let pokemonPicture = data.cards[i].imageUrl;
+    let pokemonId = data.cards[i].id;
     //console.log(pokemonPicture);
     $('#response-container').append(
-      "<div class='col-lg-2'><img src='" + pokemonPicture + "' id='choosePokemon' data-toggle='modal' data-target='#exampleModal' /> </div>"
-    )
-  }
-};
+      "<div class='col-lg-2'><button value='" + pokemonId + "' class='pokemonInfo' type='button' data-toggle='modal' data-target='#myModal'><img class='pokemon' src='" + pokemonPicture + "' /></button></div>"
+    )}
 
-//Modal 
-
-
-const pokemon = document.getElementById('choosePokemon');
-function openPokemonInfo() {
-  
+    $('.pokemonInfo').click(function() {
+      $('.modal-body').empty(); 
+      const pokeId = $(this).val();
+      fetch(`https://api.pokemontcg.io/v1/cards/${pokeId}`)
+      .then((response) => {
+        return response.json();
+      })
+      .then((pokedata) => {
+        //console.log(pokedata);
+        let poke = pokedata.card;
+        $('.modal-title').append('<h3>' + poke.name + '</h3>')
+        $('.modal-body').append('<img src="' + poke.imageUrl +'" /><p><strong>Types:</strong> ' + poke.types[0] + '</p><p><strong>Subtype:</strong> ' + poke.subtype + '</p><p><strong>Series:</strong> ' + poke.series + '</p><p><strong>Set:</strong> ' + poke.set + '</p>');
+        })
+      .catch((err) => {
+          alert(err);
+      });
+      })
+})
+.catch((err) => {
+  alert(err);
+});
 }
